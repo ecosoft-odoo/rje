9655f214a4dc5f9b50281df6f98fa3f2ce980357
Pat <patthamac.ecosoft.co.th>
2017-09-24 20:16:26 +0700

    [New] RJE :: Purchase extension Module

---
+++ specific/rje_purchase/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'RJE :: Purchase extension',
+    'version': '1.0',
+    'category': 'Purchase',
+    'summary': '',
+    'description': '',
+    'author': 'Patthama C.',
+    'website': 'http://www.ecosoft.co.th',
+    'images': [],
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/purchase_view.xml',
+    ],
+    'installable': True,
+    'active': True,
+}
---
+++ specific/rje_purchase/models/purchase.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    attention = fields.Char(
+        string='Attention',
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)],
+                'sent': [('readonly', False)]},
+    )
