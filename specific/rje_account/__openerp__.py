aa98fa242bc905a59770275ce539feafcb888e34
Pat <patthamac.ecosoft.co.th>
2017-09-25 22:03:40 +0700

    [New] RJE :: Account extension Module

---
+++ specific/rje_account/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'RJE :: Account extension',
+    'version': '1.0',
+    'category': 'Account',
+    'summary': '',
+    'description': '',
+    'author': 'Patthama C.',
+    'website': 'http://www.ecosoft.co.th',
+    'images': [],
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_view.xml',
+    ],
+    'installable': True,
+    'active': True,
+}
