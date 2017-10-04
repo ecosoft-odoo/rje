a80b568c3d6bc866a4e07883f9534cb0506c7c5b
Pat <patthamac.ecosoft.co.th>
2017-10-04 13:43:18 +0700

    [New] RJE :: Warehouse extension Module

---
+++ specific/rje_warehouse/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'RJE :: Warehouse extension',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'summary': '',
+    'description': '',
+    'author': 'Patthama C.',
+    'website': 'http://www.ecosoft.co.th',
+    'images': [],
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_view.xml',
+    ],
+    'installable': True,
+    'active': True,
+}
---
+++ specific/rje_warehouse/models/stock.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+
+
+#class Stock(models.Model):
+#    _inherit = 'stock'
+
+    #attention = fields.Char(
+    #    string='Attention',
+    #    track_visibility='onchange',
+    #    readonly=True,
+    #    states={'draft': [('readonly', False)],
+    #            'sent': [('readonly', False)]},
+    #)
