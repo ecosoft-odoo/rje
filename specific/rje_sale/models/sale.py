76f794646d648a051db142284123f13e3a70a345
Pat <patthamac.ecosoft.co.th>
2017-11-28 17:29:40 +0700

    [Update] rje sale module

--- specific/rje_sale/models/sale.py
+++ specific/rje_sale/models/sale.py
@@ -30,7 +30,16 @@ class SaleOrder(models.Model):
         string='Drwaing Number',
         related='order_line.drawing_number',
     )
-    approved_employee_id = fields.Many2one('hr.employee',
+    amount_discount = fields.Float(
+        string='Discount (Amt.)',
+        related='order_line.amount_discount',
+    )
+    percent_discount = fields.Float(
+        string='Discount (%)',
+        related='order_line.percent_discount',
+    )
+    approved_employee_id = fields.Many2one(
+        'hr.employee',
         string='Approved by',
     )
 
@@ -40,3 +49,28 @@ class SaleOrderLine(models.Model):
     drawing_number = fields.Char(
         string='Drawing Number',
     )
+    discount = fields.Float(
+        digits=(16,20),
+    )
+    amount_discount = fields.Float(
+        string='Discount (Amt.)',
+    )
+    percent_discount = fields.Float(
+        string='Discount (%)',
+    )
+
+    @api.onchange('amount_discount', 'price_unit', 'product_uom_qty')
+    def _onchange_amount_discount(self):
+        if self._context.get('by_amount_discount', False):
+            self.percent_discount = False
+            price_subtotal = self.price_unit * self.product_uom_qty
+            if not price_subtotal:
+                self.discount = 0.0
+            else:
+                self.discount = self.amount_discount / price_subtotal * 100.0
+
+    @api.onchange('percent_discount')
+    def _onchange_discount_percent(self):
+        if self._context.get('by_percent_discount', False):
+            self.amount_discount = False
+            self.discount = self.percent_discount
